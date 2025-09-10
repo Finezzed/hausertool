33b4bdeec9a107bfdaa19f32c6a9045a47922ba5
Finezzed <muellersebastian2002@gmail.com>
2025-09-10 20:40:49 +0200

    Start

---
+++ src/main/java/de/immotool/haeusertool/HaeusertoolApplication.java
@@ -0,0 +1,13 @@
+package de.immotool.haeusertool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaeusertoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaeusertoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/immotool/haeusertool/HaeusertoolApplicationTests.java
@@ -0,0 +1,13 @@
+package de.immotool.haeusertool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaeusertoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
